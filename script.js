1cc340d0f48f18b61134a7eed57999917a6357e3
Alex Castillo <wikitelmex@gmail.com>
2021-09-10 21:16:15 -0600

    Update script.js
    - linters fixed

--- script.js
+++ script.js
@@ -1,60 +1,58 @@
-
-
 const mockData = [
-    {
-        name: 'Peter',
-        title: 'MVP Microsoft',
-        imageURL: 'https://thumbs.dreamstime.com/b/old-man-suit-talking-portrait-senior-caucasian-businessman-white-isolated-background-old-man-suit-talking-113566335.jpg',
-        description: 'Bekcham Professor of Entrepreneurial Legal studies ag Harvard Law School.'
-    },
-    {
-        name: 'Alex',
-        title: 'MVP Azure Certified',
-        imageURL: 'https://thumbs.dreamstime.com/b/old-man-suit-talking-portrait-senior-caucasian-businessman-white-isolated-background-old-man-suit-talking-113566335.jpg',
-        description: 'Bekcham Professor of Entrepreneurial Legal studies ag Harvard Law School.'
-    }
-]
+  {
+    name: 'Peter',
+    title: 'MVP Microsoft',
+    imageURL: 'https://thumbs.dreamstime.com/b/old-man-suit-talking-portrait-senior-caucasian-businessman-white-isolated-background-old-man-suit-talking-113566335.jpg',
+    description: 'Bekcham Professor of Entrepreneurial Legal studies ag Harvard Law School.',
+  },
+  {
+    name: 'Alex',
+    title: 'MVP Azure Certified',
+    imageURL: 'https://thumbs.dreamstime.com/b/old-man-suit-talking-portrait-senior-caucasian-businessman-white-isolated-background-old-man-suit-talking-113566335.jpg',
+    description: 'Bekcham Professor of Entrepreneurial Legal studies ag Harvard Law School.',
+  },
+];
 
 const cardsContainer = document.querySelector('.presenters');
 
 function loadCards(cardsData = mockData) {
-    cardsData.forEach((element) => {
-        const card = document.createElement('div');
-        card.className = 'card mb-3 border-0 mw-500';
-        const subCard = document.createElement('div');
-        subCard.className = 'd-flex g-0 flex-nowrap';
-        const imgContainer = document.createElement('div');
-        imgContainer.className = 'card-chess';
-        const img = document.createElement('img');
-        img.className = 'card-speaker img-fluid rounded-start';
-        const cardBody = document.createElement('div');
-        cardBody.className = 'card-body';
-        const bodyTitle = document.createElement('h5');
-        bodyTitle.className = 'card-title';
-        bodyTitle.innerText = element.name;
-        const bodyTextTitle = document.createElement('p');
-        bodyTextTitle.className = 'card-text text-primary';
-        bodyTextTitle.innerText = element.title;
-        const bodyTextDescription = document.createElement('p');
-        bodyTextDescription.className = 'card-text';
-        bodyTextDescription.innerText = element.description;
-        
-        cardBody.appendChild(bodyTitle);
-        cardBody.appendChild(bodyTextTitle);
-        cardBody.appendChild(bodyTextDescription);
+  cardsData.forEach((element) => {
+    const card = document.createElement('div');
+    card.className = 'card mb-3 border-0 mw-500';
+    const subCard = document.createElement('div');
+    subCard.className = 'd-flex g-0 flex-nowrap';
+    const imgContainer = document.createElement('div');
+    imgContainer.className = 'card-chess';
+    const img = document.createElement('img');
+    img.className = 'card-speaker img-fluid rounded-start';
+    const cardBody = document.createElement('div');
+    cardBody.className = 'card-body';
+    const bodyTitle = document.createElement('h5');
+    bodyTitle.className = 'card-title';
+    bodyTitle.innerText = element.name;
+    const bodyTextTitle = document.createElement('p');
+    bodyTextTitle.className = 'card-text text-primary';
+    bodyTextTitle.innerText = element.title;
+    const bodyTextDescription = document.createElement('p');
+    bodyTextDescription.className = 'card-text';
+    bodyTextDescription.innerText = element.description;
 
-        img.src = element.imageURL;
-        imgContainer.appendChild(img);
+    cardBody.appendChild(bodyTitle);
+    cardBody.appendChild(bodyTextTitle);
+    cardBody.appendChild(bodyTextDescription);
 
-        subCard.appendChild(imgContainer);
-        subCard.appendChild(cardBody);
+    img.src = element.imageURL;
+    imgContainer.appendChild(img);
 
-        card.appendChild(subCard);
+    subCard.appendChild(imgContainer);
+    subCard.appendChild(cardBody);
 
-        cardsContainer.appendChild(card);
-    });
+    card.appendChild(subCard);
+
+    cardsContainer.appendChild(card);
+  });
 }
 
 window.onload = function () {
-    loadCards();
-}
+  loadCards();
+};
